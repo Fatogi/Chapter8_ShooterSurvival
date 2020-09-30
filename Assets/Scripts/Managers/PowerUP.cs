7d5fd018f78f3fda7bd88710e600b42ea1ea7508
Fatogi <ogiggsm@gmail.com>
2020-09-30 18:34:44 +0800

    FINISH

---
+++ Assets/Scripts/Managers/PowerUP.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUP : MonoBehaviour
+    
+{
+    public GameObject PoP;
+    public PlayerMovement player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Set player dalam range
+        if (other.gameObject == PoP)
+        {
+            player.PoweerUP();
+            Destroy(gameObject);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/PowerUPHeal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUPHeal : MonoBehaviour
+{
+    public GameObject PoP;
+    public PlayerHealth player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Set player dalam range
+        if (other.gameObject == PoP)
+        {
+            player.PowerUP();
+            Destroy(gameObject);
+
+        }
+    }
+}
